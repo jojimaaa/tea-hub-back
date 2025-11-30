eff68f1a330aab68ca2b18116a4ba2ad025e9097
Enzo <enzoakiogoro@gmail.com>
2025-11-30 14:04:40 -0300

    chore: format database.py

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from fastapi import Depends
 from typing import Annotated
 
-URL_DATABASE = 'postgresql://u_grupo01:grupo01@200.144.245.12:45432/db_grupo01'
+URL_DATABASE = "postgresql://u_grupo01:grupo01@200.144.245.12:45432/db_grupo01"
 
 engine = create_engine(URL_DATABASE)
 
@@ -12,11 +12,13 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
+
+
 db_dependency = Annotated[Session, Depends(get_db)]
