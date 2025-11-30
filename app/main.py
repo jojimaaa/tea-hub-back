5c9a9ae8393b7147681ba031d510555ef7a0635a
jojimaaa <enzo.jojima@usp.br>
2025-11-30 18:14:16 -0300

    chore: ajusted cors for develop and prod deployments

--- app/main.py
+++ app/main.py
@@ -11,8 +11,9 @@ origins = [
     "http://localhost:3001",
     "http://192.168.56.1:3000",
     "http://192.168.56.1:3001",
-    "tea-hub-front-dev-git-feat-forumlist-enzos-projects-b920b2f9.vercel.app",
-    "tea-hub-front-qmigvonx3-enzos-projects-b920b2f9.vercel.app",
+    "https://tea-hub-front-dev-git-develop-enzos-projects-b920b2f9.vercel.app", # develop
+    "https://tea-hub-front-dev.vercel.app",
+    "https://tea-hub-front-dev-git-feat-forumlist-enzos-projects-b920b2f9.vercel.app"
 ]
 
 app.add_middleware(
